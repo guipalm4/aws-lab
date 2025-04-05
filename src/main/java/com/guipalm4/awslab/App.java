33be866aa1b100600edf138b98197e8834f9053e
guipalm4 <gomes.lmc@gmail.com>
2025-04-05 13:12:10 -0300

    feat - add `Spring-Boot`

---
+++ src/main/java/com/guipalm4/awslab/App.java
@@ -0,0 +1,12 @@
+package com.guipalm4.awslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
